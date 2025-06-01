6bb7c6a6b02302c082762e7b7cb2a1685cb45a6c
Rakhshan Ali <alirakhshan7070@gmail.com>
2025-06-01 17:16:38 +0530

    cuscoi: move motorola.hardware.camera.desktop to common tree

--- cuscoi-vendor.mk
+++ cuscoi-vendor.mk
@@ -3212,7 +3212,6 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/cuscoi/proprietary/vendor/etc/display/qdcm_calib_data_vtdr6130_amoled_qsync_video_mode_dsi_visionox_panel_with_DSC.json:$(TARGET_COPY_OUT_VENDOR)/etc/display/qdcm_calib_data_vtdr6130_amoled_qsync_video_mode_dsi_visionox_panel_with_DSC.json \
     vendor/motorola/cuscoi/proprietary/vendor/etc/display/qdcm_calib_data_vtdr6130_amoled_video_mode_dsi_visionox_panel_with_DSC.json:$(TARGET_COPY_OUT_VENDOR)/etc/display/qdcm_calib_data_vtdr6130_amoled_video_mode_dsi_visionox_panel_with_DSC.json \
     vendor/motorola/cuscoi/proprietary/vendor/etc/init/android.hardware.biometrics.fingerprint@2.1-goodixservice.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.biometrics.fingerprint@2.1-goodixservice.rc \
-    vendor/motorola/cuscoi/proprietary/vendor/etc/init/motorola.hardware.camera.desktop@2.0-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/motorola.hardware.camera.desktop@2.0-service.rc \
     vendor/motorola/cuscoi/proprietary/vendor/etc/init/motorola.hardware.camera.imgtuner.aidl-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/motorola.hardware.camera.imgtuner.aidl-service.rc \
     vendor/motorola/cuscoi/proprietary/vendor/etc/init/vendor.qti.camera.provider@2.7-service_64.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.qti.camera.provider@2.7-service_64.rc \
     vendor/motorola/cuscoi/proprietary/vendor/etc/libnfc-hal-st.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-hal-st.conf \
@@ -3479,8 +3478,6 @@ PRODUCT_PACKAGES += \
     libthreadutils \
     libundistort_impl \
     libvidhance \
-    motorola.hardware.camera.desktop@1.0 \
-    motorola.hardware.camera.desktop@2.0 \
     motorola.hardware.camera.imgtuner.aidl-V1-ndk_platform \
     sensors.moto \
     vendor.goodix.hardware.biometrics.fingerprint@2.1 \
@@ -3493,6 +3490,5 @@ PRODUCT_PACKAGES += \
     motorola.hardware.camera.imgtuner.aidl.xml \
     STFlashTool \
     android.hardware.biometrics.fingerprint@2.1-goodixservice \
-    motorola.hardware.camera.desktop@2.0-service \
     motorola.hardware.camera.imgtuner.aidl-service \
     vendor.qti.camera.provider@2.7-service_64
